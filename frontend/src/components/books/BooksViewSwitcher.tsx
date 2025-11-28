67815a89a811ef5fe77330c3e66aef0bb5966392
Hayeon Baek <bhy0512@gmail.com>
2025-11-28 14:38:09 +0900

    feat: 도서 목록 뷰 전환 기능 구현 (리스트/그리드)
    - BooksViewSwitcher 컴포넌트 구현 (리스트/그리드 전환 버튼)
    - 뷰 모드를 URL 쿼리스트링에 저장 및 동기화
    - BookItem 컴포넌트에 view props 추가 (동적 스타일링)
    - BooksList 컴포넌트에 뷰 모드 상태 관리 기능 추가
    - 리스트 뷰: 1열, 그리드 뷰: 4열 레이아웃 구현
    - QUERYSTRING 상수에 VIEW 추가

--- frontend/src/components/books/BooksViewSwitcher.tsx
+++ frontend/src/components/books/BooksViewSwitcher.tsx
@@ -1,15 +1,60 @@
 import styled from 'styled-components';
+import Button from '../common/Button';
+import { FaList, FaTh } from 'react-icons/fa';
+import { useSearchParams } from 'react-router-dom';
+import { QUERYSTRING } from '../../constants/querystring';
+import { useEffect } from 'react';
 
-interface Props {}
+const viewOptions = [
+  { value: 'list', icon: <FaList /> },
+  { value: 'grid', icon: <FaTh /> },
+];
 
-function BooksViewSwitcher(props: Props) {
+export type ViewMode = 'list' | 'grid';
+
+function BooksViewSwitcher() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleSwitch = (value: ViewMode) => {
+    const newSearchParams = new URLSearchParams(searchParams);
+
+    newSearchParams.set(QUERYSTRING.VIEW, value);
+    setSearchParams(newSearchParams);
+  };
+
+  useEffect(() => {
+    if (!searchParams.get(QUERYSTRING.VIEW)) {
+      handleSwitch('grid');
+    }
+  }, []);
+
   return (
     <BooksViewSwitcherStyle>
-      <h1>BooksViewSwitcher</h1>
+      {viewOptions.map((option) => (
+        <Button
+          key={option.value}
+          size="medium"
+          scheme={
+            searchParams.get(QUERYSTRING.VIEW) === option.value
+              ? 'primary'
+              : 'normal'
+          }
+          onClick={() => handleSwitch(option.value as ViewMode)}
+        >
+          {option.icon}
+        </Button>
+      ))}
     </BooksViewSwitcherStyle>
   );
 }
 
-const BooksViewSwitcherStyle = styled.div``;
+const BooksViewSwitcherStyle = styled.div`
+  display: flex;
+  gap: 8px;
+
+  svg {
+    fill: #fff;
+  }
+`;
 
 export default BooksViewSwitcher;
